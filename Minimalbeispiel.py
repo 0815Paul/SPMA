c31a7d387df120db21725a5c1cb246879525e7e0
Paul Wilhelm <paul.wilhelm@stud.htwk-leipzig.de>
2024-06-11 18:41:03 +0200

    new Version of Minimalbeispiel, but does not work!

--- Minimalbeispiel.py
+++ Minimalbeispiel.py
@@ -1,76 +1,52 @@
 import pyomo.environ as pyo
 import matplotlib.pyplot as plt
+from pyomo.environ import Binary, NonNegativeReals
+import sys
 
 # Szenarien und Wahrscheinlichkeiten
 scenarios = [1, 2, 3]
 timesteps = [1, 2, 3, 4, 5]
 probabilities = {1: 1/3, 2: 1/3, 3: 1/3}
 demand = {
-    (1, 1): 50, (1, 2): 60, (1, 3): 55, (1, 4): 50, (1, 5): 45,
+    (1, 1): 90, (1, 2): 96, (1, 3): 92, (1, 4): 94, (1, 5): 88,
     (2, 1): 75, (2, 2): 85, (2, 3): 80, (2, 4): 75, (2, 5): 70,
     (3, 1): 100, (3, 2): 100, (3, 3): 95, (3, 4): 120, (3, 5): 90
 }
 
 # Parameter
+gas_price = {1:9.22, 2:8.84, 3:8.67, 4:8.12, 5:9.49}
+power_price = {1:90.8, 2:96.2, 3:92.3, 4:88.7, 5:91.1}
+heat_price = {1:50, 2:50, 3:50, 4:50, 5:50}
 
-# Prices in €/MWh
-gas_price = {1:9.22, 2:8.84, 3:8.67, 4:8.12, 5:9.49} #per MWh
-#gas_price_const = 8.5 #per MWh
-power_price = {1:90.8, 2:96.2, 3:92.3, 4:88.7, 5:91.1} #per MWh
-#power_price_const = 90 #per MWh	
+# CHP parameters
+chp_power_max = 63
+chp_power_min = 10
+chp_gas_max = 160
+chp_gas_min = 66
+chp_heat_max = 82
+chp_heat_min = 41
 
-# CHP
-chp_power_max = 62.424 # in kW
-chp_power_min = 13.410 # in kW
-chp_gas_max = 156.328 # in kW
-chp_gas_min = 64.794 # in kW
-chp_heat_max = 82.000 # in kW
-chp_heat_min = 41.000 # in kW
+# Heat Storage parameters
+storage_heat_max = 50
+storage_heat_min = 0
+storage_content_max = 150
 
-
-
-# Modell erstellen
+# Model erstellen
 model = pyo.ConcreteModel()
 
-# CHP
+# Variables
 model.chp_bin = pyo.Var(scenarios, timesteps, within=Binary)
 model.chp_gas = pyo.Var(scenarios, timesteps, within=NonNegativeReals)
 model.chp_power = pyo.Var(scenarios, timesteps, within=NonNegativeReals)
 model.chp_heat = pyo.Var(scenarios, timesteps, within=NonNegativeReals)
 
-# Heat Storage
-model.storage_heat_charging = pyo.Var(scenarios, timesteps, within=NonNegativeReals)
-model.storage_heat_discharging = pyo.Var(scenarios, timesteps, within=NonNegativeReals)
+model.storage_heat = pyo.Var(scenarios, timesteps, within=NonNegativeReals)
 model.storage_content = pyo.Var(scenarios, timesteps, within=NonNegativeReals)
+model.storage_bin_discharging = pyo.Var(scenarios, timesteps, within=Binary)
+model.storage_bin_charging = pyo.Var(scenarios, timesteps, within=Binary)
 
-
-# bis hierhin bin ich gekommen
-# ----------------------------------------------------------------------------------------------------------------------------
-
-c_elek = 20  # Kosten des Strombezugs
-c_gas = 10   # Kosten des Gasbezugs
-c_bhkw = 5   # Kosten der BHKW-Wärmeerzeugung
-c_kessel = 8 # Kosten der Kessel-Wärmeerzeugung
-c_speicher = 2 # Kosten der Wärmespeicherung
-y_bhkw_max = 80
-y_kessel_max = 90
-z_speicher_max = 50
-
-
-# Variablen
-model.x_elek = pyo.Var(scenarios, timesteps, within=pyo.NonNegativeReals)
-model.x_gas = pyo.Var(scenarios, timesteps, within=pyo.NonNegativeReals)
-model.y_bhkw = pyo.Var(scenarios, timesteps, within=pyo.NonNegativeReals)
-model.y_kessel = pyo.Var(scenarios, timesteps, within=pyo.NonNegativeReals)
-model.z_speicher = pyo.Var(scenarios, timesteps, within=pyo.NonNegativeReals)
-
-# Zielfunktion
-def objective_rule(model):
-    return sum(probabilities[i] * (c_elek * model.x_elek[i,t] + c_gas * model.x_gas[i,t] + c_bhkw * model.y_bhkw[i,t] + c_kessel * model.y_kessel[i,t] + c_speicher * model.z_speicher[i,t]) for i in scenarios for t in timesteps)
-model.objective = pyo.Objective(rule=objective_rule, sense=pyo.minimize)
-
-# Nebenbedingungen
-
+# Constraints
+# CHP Constraints
 def power_max_rule(model, i, t):
     return model.chp_power[i,t] <= chp_power_max * model.chp_bin[i,t]
 model.power_max = pyo.Constraint(scenarios, timesteps, rule=power_max_rule)
@@ -83,55 +59,85 @@ def gas_depends_on_power_rule(model, i, t):
     a = (chp_gas_max - chp_gas_min) / (chp_power_max - chp_power_min)
     b = chp_gas_max - a * chp_power_max
     return model.chp_gas[i,t] == a * model.chp_power[i,t] + b * model.chp_bin[i,t]
+model.gas_depends_on_power = pyo.Constraint(scenarios, timesteps, rule=gas_depends_on_power_rule)
 
 def heat_depends_on_power_rule(model, i, t):
     a = (chp_heat_max - chp_heat_min) / (chp_power_max - chp_power_min)
     b = chp_heat_max - a * chp_power_max
     return model.chp_heat[i,t] == a * model.chp_power[i,t] + b * model.chp_bin[i,t]
+model.heat_depends_on_power = pyo.Constraint(scenarios, timesteps, rule=heat_depends_on_power_rule)
+
+# Heat Storage Constraints
+def storage_balance_rule(model, i, t):
+    if t == 1:
+        return model.storage_content[i,t] == 0 - model.storage_heat[i,t]
+    else:
+        return model.storage_content[i,t] == model.storage_content[i,t-1] - model.storage_heat[i,t] 
+model.storage_balance = pyo.Constraint(scenarios, timesteps, rule=storage_balance_rule)
+
+def storage_initial_rule(model, i):
+    return model.storage_content[i,1] == 0
+model.storage_initial = pyo.Constraint(scenarios, rule=storage_initial_rule)
+
+def max_storage_rule(model, i, t):
+    return model.storage_content[i,t] <= storage_content_max
+model.max_storage = pyo.Constraint(scenarios, timesteps, rule=max_storage_rule)
+
+def min_storage_rule(model, i, t):
+    return model.storage_content[i,t] >= storage_heat_min
+model.min_storage = pyo.Constraint(scenarios, timesteps, rule=min_storage_rule)
+
+def storage_heat_charging_rule(model, i, t):
+    return model.storage_heat[i,t] <= -storage_heat_max
+model.storage_heat_charging_rule = pyo.Constraint(scenarios, timesteps, rule=storage_heat_charging_rule)
+
+def storage_heat_discharging_rule(model, i, t):
+    return model.storage_heat[i,t] <= +storage_heat_max
+model.storage_heat_discharging_rule = pyo.Constraint(scenarios, timesteps, rule=storage_heat_discharging_rule)
 
 
+def storage_bin_rule(model, i, t):
+    return model.storage_bin_discharging[i,t] + model.storage_bin_charging[i,t] <= 1
+model.storage_bin_rule = pyo.Constraint(scenarios, timesteps, rule=storage_bin_rule)
+ 
+# Heat Balance Constraint
 
 def demand_constraint_rule(model, i, t):
-    return model.y_bhkw[i,t] + model.y_kessel[i,t] + model.z_speicher[i,t] >= demand[(i, t)]
+    return model.chp_heat[i,t] + model.storage_heat_discharging[i,t] >= demand[(i, t)]
 model.demand_constraint = pyo.Constraint(scenarios, timesteps, rule=demand_constraint_rule)
 
-def bhkw_limit_rule(model, i, t):
-    return model.y_bhkw[i,t] <= y_bhkw_max
-model.bhkw_limit = pyo.Constraint(scenarios, timesteps, rule=bhkw_limit_rule)
-
-def kessel_limit_rule(model, i, t):
-    return model.y_kessel[i,t] <= y_kessel_max
-model.kessel_limit = pyo.Constraint(scenarios, timesteps, rule=kessel_limit_rule)
-
-def speicher_limit_rule(model, i, t):
-    return model.z_speicher[i,t] <= z_speicher_max
-model.speicher_limit = pyo.Constraint(scenarios, timesteps, rule=speicher_limit_rule)
+# Objective function
+def objective_function(model):
+    return sum(probabilities[s] * sum(
+        gas_price[t] * model.chp_gas[s, t] -  # Kosten für Gas
+        heat_price[t] * model.storage_heat_discharging[s, t]  # Einnahmen aus Verkauf von Wärme
+        for t in timesteps) for s in scenarios)
+model.obj = pyo.Objective(rule=objective_function, sense=pyo.minimize)
 
 # Solver
 solver = pyo.SolverFactory('gurobi')
 result = solver.solve(model)
 
 # Ergebnisse anzeigen
-#model.display()
+model.display()
 
-# Ergebnisse extrahieren
-x_elek_values = {(i, t): pyo.value(model.x_elek[i, t]) for i in scenarios for t in timesteps}
-x_gas_values = {(i, t): pyo.value(model.x_gas[i, t]) for i in scenarios for t in timesteps}
-y_bhkw_values = {(i, t): pyo.value(model.y_bhkw[i, t]) for i in scenarios for t in timesteps}
-y_kessel_values = {(i, t): pyo.value(model.y_kessel[i, t]) for i in scenarios for t in timesteps}
-z_speicher_values = {(i, t): pyo.value(model.z_speicher[i, t]) for i in scenarios for t in timesteps}
+# Extract results for plotting
+chp_power_results = {s: [pyo.value(model.chp_power[s, t]) for t in timesteps] for s in scenarios}
+chp_heat_results = {s: [pyo.value(model.chp_heat[s, t]) for t in timesteps] for s in scenarios}
+heat_demand_results = {s: [demand[(s, t)] for t in timesteps] for s in scenarios}
+storage_heat_discharging_results = {s: [pyo.value(model.storage_heat_discharging[s, t]) for t in timesteps] for s in scenarios}
+storage_content_results = {s: [pyo.value(model.storage_content[s, t]) for t in timesteps] for s in scenarios}
 
-# Ergebnisse visualisieren
-for i in scenarios:
+# Plotting CHP heat output, heat demand, and storage heat discharging for each scenario in separate plots
+for s in scenarios:
     plt.figure(figsize=(12, 6))
-    plt.plot(timesteps, [x_elek_values[(i, t)] for t in timesteps], label='Strombezug')
-    plt.plot(timesteps, [x_gas_values[(i, t)] for t in timesteps], label='Gasbezug')
-    plt.plot(timesteps, [y_bhkw_values[(i, t)] for t in timesteps], label='BHKW Wärmeerzeugung')
-    plt.plot(timesteps, [y_kessel_values[(i, t)] for t in timesteps], label='Kessel Wärmeerzeugung')
-    plt.plot(timesteps, [z_speicher_values[(i, t)] for t in timesteps], label='Wärmespeicherung')
-    plt.xlabel('Zeitschritt')
-    plt.ylabel('Energie (Einheiten)')
-    plt.title(f'Szenario {i}')
+    plt.plot(timesteps, chp_heat_results[s], label=f'Scenario {s} - Heat Output CHP', linestyle='--', marker='x')
+    plt.plot(timesteps, heat_demand_results[s], label=f'Scenario {s} - Heat Demand Consumer', linestyle=':', marker='s')
+    plt.plot(timesteps, storage_heat_discharging_results[s], label=f'Scenario {s} - Storage Heat Discharging', linestyle='-.', marker='o')
+    plt.plot(timesteps, storage_content_results[s], label=f'Scenario {s} - Storage Content', linestyle='-', marker='d')
+    plt.xlabel('Timesteps')
+    plt.ylabel('Output / Demand (kW)')
+    plt.title(f'Heat Output, Heat Demand, Storage Discharging, and Storage Content for Scenario {s}')
     plt.legend()
     plt.grid(True)
     plt.show()
