98ef5ccbb8af7ae106af436e5629ce5e194a46fe
Paul Wilhelm <paul.wilhelm@stud.htwk-leipzig.de>
2024-08-23 13:24:12 +0200

    Added stochastic, code worked with ef, but results of vars couldnt be computed

--- 4DEnergy.py
+++ 4DEnergy.py
@@ -1,8 +1,19 @@
+import sys
 import pandas as pd
+import pyomo.environ as pyo
 
 from pyomo.opt import SolverFactory
 from pyomo.environ import *
 from pyomo.network import *
+
+from mpisppy.opt.ph import PH
+from mpisppy.opt.lshaped import LShapedMethod
+from mpisppy.utils.sputils import scenario_tree
+from mpisppy.utils import config
+import mpisppy.utils.sputils as sputils
+import mpisppy.utils.solver_spec as solver_spec
+
+
 
 import assets.chp as chp
 import assets.boiler as boiler
@@ -20,7 +31,6 @@ POWER_PRICE = 0.251 # €/kWh (el)
 HEAT_PRICE = 0.105 # €/kWh (th)
 CALORIFIC_VALUE_NGAS = 10 # kWh/m3
 
-
 # CHP 
 CHP_BONUS_SELF_CONSUMPTION= 0.08  # €/kWhel
 CHP_BONUS= 0.16  # €/kWhel
@@ -30,9 +40,9 @@ AVOIDED_GRID_FEES= 0.0097  # €/kWhel
 SHARE_SELF_CONSUMPTION= 0.03 # %
 SHARE_FEED_IN= 0.97 # %
 
-
 # Costs
 MAINTENANCE_COSTS = 1.8 # €/kWh (HS)
+
 
 class Model:
     """Model class."""
@@ -49,8 +59,9 @@ class Model:
         self.solver = SolverFactory(solver_name)
         
         for key in kwargs:
-            self.solver.options[key] = kwargs[key]
-    
+            self.solver.options[key] = kwargs[key]    
+
+
     def load_timeseries_data(self):
         """Load timeseries data from file."""
         self.timeseries_data = DataPortal()
@@ -61,66 +72,19 @@ class Model:
             param='heat_demand'
         )
 
-        #_____ In case of power price is not constant _____#
-
-        # self.timeseries_data.load(
-        #     filename=PATH_IN + '/prices/power_price.csv',
-        #     index='t',
-        #     param='power_price'
-        # )
-
-        #_________________________________________________#
-
-        #_____ In case of heat price is not constant _____#
-
-        # self.timeseries_data.load(
-        #     filename=PATH_IN + '/prices/heat_price.csv',
-        #     index='t',
-        #     param='heat_price'
-        # )
-
-        #_________________________________________________#
-
-        #_____ In case of gas price is not constant _____#
-    
-        # self.timeseries_data.load(
-        #     filename=PATH_IN + '/prices/gas_price.csv',
-        #     index='t',
-        #     param='gas_price'
-        # )
-
-        #_________________________________________________#
-
-
     def add_components(self):
         """Add components to the model."""
 
         # Sets
         self.model.t = Set(ordered=True)
-
+    
         # Parameters
         self.model.GAS_PRICE = Param(initialize=GAS_PRICE)
         self.model.POWER_PRICE = Param(initialize=POWER_PRICE)
         self.model.HEAT_PRICE = Param(initialize=HEAT_PRICE)
         self.model.heat_demand = Param(self.model.t)
 
-        #_____ In case of power price is not constant _____#
-        
-        #self.model.power_price = Param(self.model.t)
 
-        #_________________________________________________#
-        
-        #_____ In case of heat price is not constant _____#
-
-        #self.model.heat_price = Param(self.model.t)
-
-        #_________________________________________________#
-
-        #_____ In case of gas price is not constant _____#
-
-        #self.model.gas_price = Param(self.model.t)
-
-        #_________________________________________________#
 
         # Assets
 
@@ -172,6 +136,19 @@ class Model:
         power_grid.add_to_model(self.model)
         heat_grid.add_to_model(self.model)
 
+        # Expressions
+
+        def first_stage_cost_rule(model):
+            return quicksum(model.chp1.gas[t] * model.price_scenario for t in model.t)
+        self.model.FirstStageCost = Expression(rule=first_stage_cost_rule)
+
+        def second_stage_cost_rule(model):
+            return 2
+        self.model.SecondStageCost = Expression(rule=second_stage_cost_rule)
+
+        # Testing 
+
+
 
     def add_objective(self):
         """Add objective function to model."""
@@ -179,11 +156,95 @@ class Model:
             rule=self.objective_expr,
             sense=minimize
         )
-    
-    def instantiate_model(self):
-        """Create a concrete instance of the model."""
-        self.instance = self.model.create_instance(self.timeseries_data)
+      
+    # def instantiate_model(self, scenario_name=None, **scenario_creator_kwargs):
+    #     if scenario_name is None:
+    #         # Fallback to deterministic model
+    #         self.instance = self.model.create_instance(self.timeseries_data)
 
+    #     else:
+    #         self.instance = self.scenario_creator(scenario_name, **scenario_creator_kwargs)
+        
+
+    ## **scenario_creator_kwargs später hinzufügen
+    def scenario_creator(self, scenario_name, use_integer=False, sense=pyo.minimize, crops_multiplier=1, num_scens=None):
+        """Create a scenario for the energy system model.
+        
+        Parameters:
+        scenario_name: str
+            Name of the scenario to construct.
+        use_integer: bool, optional
+            If True, restricts variables to be integer. Default is False.
+        sense: int, optional
+            Model sense (minimization or maximization). Must be either pyo.minimize or pyo.maximize. Default is pyo.minimize.
+        crops_multiplier: int, optional
+            Factor to control scaling. There will be three times this many crops. Default is 1.
+        num_scens: int, optional
+            Number of scenarios. We use it to compute _mpisppy_probability. Default is None.
+        seedoffset: int, optional
+            Used by confidence interval code. Default is 0.
+        """
+        # My abstract model
+        model = self.model
+
+        # Brauche ich nicht
+        # scenario_base_name = scenario_name.rstrip('1234567890')
+        # print("scenario_base_name=", scenario_base_name)
+        
+
+        # Use the scenario_creator function to create a stochastic model
+        scennum = sputils.extract_num(scenario_name)
+        basenames = ['FirstScenario', 'SecondScenario', 'ThirdScenario']
+        basenum = scennum % 3
+        groupnum = scennum // 3
+        scenname = basenames[basenum] + str(groupnum)
+        print(f"Creating scenario {scenname} from {scenario_name}")
+
+        scenario_base_name = scenname.rstrip('1234567890')
+        print(f"Creating scenario base name {scenario_base_name} from {scenname}")
+
+        # Check for minimization vs. maximization
+        if sense not in [pyo.minimize, pyo.maximize]:
+            raise ValueError("Model sense Not recognized")
+            
+        ##### Create the concrete model object #####
+        
+        """Hier müssen meine konkreten Daten rein"""
+        #____________ Add Stochastic ____________
+       
+        # Define the price scenarios for the gas price
+        price_scenario = {
+            'FirstScenario': {'GAS_PRICE': 0.1541},
+            'SecondScenario': {'GAS_PRICE': 0.1542},
+            'ThirdScenario': {'GAS_PRICE': 0.1543}
+}
+
+        def price_init(m ,t):
+            price_base_name = 'GAS_PRICE'
+            return price_scenario[scenario_base_name][price_base_name]
+        
+        self.model.price_scenario = Param(within=NonNegativeReals, initialize=price_init, mutable=True)
+
+        instance = model.create_instance(self.timeseries_data)
+
+
+        if num_scens is not None:
+            instance._mpisppy_probability = 1 / num_scens
+        
+        # Varlist ist die Liste der Variablen, die an den Root-Knoten angehängt werden, 
+        # also quasi Entscheidung der Ersten Stufe, welche Ausgang für die Zweite Stufe sind
+        
+        # Variable mit Index t anlegen 
+        varlist = [instance.chp1.gas[t] for t in instance.t]
+        sputils.attach_root_node(instance, instance.FirstStageCost, varlist)
+
+
+
+        return instance
+
+
+
+        #_______________________________________
     
     def expand_arcs(self):
         """Expands arcs and generate connection constraints."""
@@ -220,16 +281,19 @@ class Model:
             destination=self.instance.chp1.gas_in
         )
 
-    def solve(self):
-        """Solve the model."""
-        self.results =self.solver.solve(
-            self.instance,
-            symbolic_solver_labels=True,
-            tee=True,
-            logfile=PATH_OUT + 'logfile.txt',
-            load_solutions=True,
-            report_timing=True,
-        )
+
+    # Noch mit einbauen später.
+
+    # def solve(self):
+    #     """Solve the model."""
+    #     self.results =self.solver.solve(
+    #         self.instance,
+    #         symbolic_solver_labels=True,
+    #         tee=True,
+    #         logfile=PATH_OUT + 'logfile.txt',
+    #         load_solutions=True,
+    #         report_timing=True,
+    #     )
     
     def write_results(self):
         """Write results to file."""
@@ -265,6 +329,7 @@ class Model:
     def objective_expr(self, model):
         """Objective function expression."""
         objective_expr = (
+            self._ComputeFirstStageCost(model) +
             self._gas_costs(model) +
             self._maintenance_costs(model) -
             self._power_revenue(model) -
@@ -272,6 +337,19 @@ class Model:
             self._chp_revenue(model)
         )
         return objective_expr
+    
+    # Implementierung der Stage Costs war falsch. Expressions für die Stage Costs wurde jetzt weiter oben im Quellcode implementiertö
+
+    def _ComputeFirstStageCost(self, model):
+        """Calculate first stage cost."""
+        firststagecost = model.FirstStageCost
+        return firststagecost
+   
+    # def _ComputeSecondStageCost(self, model):
+    #     """Calculate second stage cost."""
+    #     secondstagecost = 0
+    #     return secondstagecost
+    # model.SecondStageCost = Expression(rule=_ComputeSecondStageCost)
 
     def _gas_costs(self, model):
         """ Calculate gas costs for CHP and Boiler."""
@@ -316,36 +394,121 @@ class Model:
         )
         return chp_revenue
 
+# ____________ Add Stochastic ____________
+    def main_no_cfg(self):
+        # Some parameters from sys.argv and some hard-wired.
+        if len(sys.argv) != 4:
+            print("usage python 4DEnergy.py {crops_multiplier} {scen_count} {solver_name}")
+            print("e.g., python 4DEnergy.py 1 3 gurobi")
+            quit()
+
+        scenario_creator = self.scenario_creator
+
+        crops_multiplier = int(sys.argv[1])
+        scen_count = int(sys.argv[2])
+        solver_name = sys.argv[3]
+        
+        scenario_creator_kwargs = {
+            "use_integer": False,
+            "crops_multiplier": crops_multiplier,
+        }
+
+        scenario_names = ['Scenario' + str(i) for i in range(scen_count)]
+
+        # _____________
+        print('Setting solver...')
+        self.set_solver(solver_name)
+
+        print('Loading timeseries data...')
+        self.load_timeseries_data()
+
+        print('Adding components...')
+        self.add_components()
+
+        print('Adding objective...')
+        self.add_objective()
+
+        # print('Creating extensive Form...')
+        # ef = sputils.create_EF(
+        #     scenario_names,
+        #     scenario_creator, # must be the concrete Model!!
+        #     scenario_creator_kwargs=scenario_creator_kwargs,
+        # )
+
+        options = {
+            "root_solver": "gurobi",
+            "sp_solver": "gurobi",
+            "sp_solver_options" : {"threads" : 2},
+            "max_iter": 10,
+        }
+
+        print('Creating L-Shaped Method...')
+        ls = LShapedMethod(options, scenario_names, scenario_creator)
+        #result = ls.lshaped_algorithm()
+
+        variables = ls.gather_var_values_to_rank0()
+        for ((scen_name, var_name), var_value) in variables.items():
+            print(scen_name, var_name, var_value)
+
+        # solver = pyo.SolverFactory(solver_name)
+        # solver.solve(ef, tee=True, symbolic_solver_labels=True, logfile=PATH_OUT + 'logfile.txt', load_solutions=True, report_timing=True)
+        
+        ls.write_tree_solution("output/data/lshaped_res_all_stages")
+        ls.write_first_stage_solution("output/data/lshaped_res_first_stage/lshaped_res_first_stage.csv")
+
+
+        # print('Write results...')
+        # self.write_results()
+        
+        return ls
+        
+
+    
+    
+# ________________________________________ 
+
+
+
 if __name__ == "__main__":
-    model = Model()
+    m = Model()
 
-    print('Setting solver...')
-    model.set_solver(
-        solver_name= 'gurobi',
-        MIPGap=0.015,
-        TimeLimit=30
-        )
+    # print('Setting solver...')
+    # m.set_solver(
+    #     solver_name= 'gurobi',
+    #     MIPGap=0.015,
+    #     TimeLimit=30
+    #     )
 
-    print('Loading timeseries data...')
-    model.load_timeseries_data()
+    # print('Loading timeseries data...')
+    # m.load_timeseries_data()
 
-    print('Adding components...')
-    model.add_components()
+    # print('Adding components...')
+    # m.add_components()
 
-    print('Adding objective...')
-    model.add_objective()
+    # print('Adding objective...')
+    # m.add_objective()
 
-    print('Instantiating model...')
-    model.instantiate_model()
+    # print('Instantiating model...')
+    # m.instantiate_model()
 
-    print('Declairing arcs...')
-    model.add_arcs()
-    model.expand_arcs()
+    # print('Declairing arcs...')
+    # m.add_arcs()
+    # m.expand_arcs()
 
     print('Solving model...')
-    model.solve()
-    
+    main_ef = m.main_no_cfg()
+
     print('Writing results...')
-    model.write_results()
-    model.save_results(PATH_OUT + 'results.csv')
+    print(f"EF objective: {pyo.value(main_ef.EF_Obj)}")
+    #sputils.write_ef_tree_solution(main_ef, "4DEnergy_ef")
+
+    #sputils.ef_ROOT_nonants_npy_serializer(main_ef, "farmer_root_nonants.npy")
+    
+    result = dict()
+    for (sname, scenario) in main_ef.items():
+        result[sname] = dict()
+        for node in scenario._mpisppy_node_list:
+            for var in node.nonant_vardata_list:
+                result[sname][var.name] = var.value
+        result[sname]['z'] = scenario.z[0].value
     
