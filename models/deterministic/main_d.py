235f944c537e517bfc132dc7067d04513a222499
Paul Wilhelm <paul.wilhelm@stud.htwk-leipzig.de>
2024-10-09 14:37:08 +0200

    Updated

--- models/deterministic/main_d.py
+++ models/deterministic/main_d.py
@@ -14,9 +14,8 @@ import json
 import os
 import re
 
-
 # Load the config.json
-with open ('../config.json', 'r') as f:
+with open('../config.json', 'r') as f:
     config = json.load(f)
 
 # Model Config
@@ -34,105 +33,66 @@ PATH_IN = os.path.join(data_path, model_config['input_path'])
 PATH_OUT = os.path.join(data_path, model_config['output_path'])
 PATH_OUT_LOGS = os.path.join(data_path, model_config['log_path'])
 PATH_OUT_TIMESERIES = os.path.join(data_path, model_config['timeseries_path'])
+PATH_OUT_OBJECTIVES = os.path.join(data_path, model_config['objectives_path'])
 
 # Heat Demand Data
 FILE_HEAT_DEMAND = global_config['heat_demand_file']
+FILE_HEAT_DEMAND_SCENARIOS = global_config['heat_demand_scenario_file']
 
 # Declare constants
-GAS_PRICE = global_config['gas_price'] # €/kWh  (HS)
-POWER_PRICE = global_config['power_price'] # €/kWh (el)
-HEAT_PRICE = global_config['heat_price'] # €/kWh (th)
-CALORIFIC_VALUE_NGAS = global_config['calorific_value_ngas'] # kWh/m3
+GAS_PRICE = global_config['gas_price']  # €/kWh  (HS)
+POWER_PRICE = global_config['power_price']  # €/kWh (el)
+HEAT_PRICE = global_config['heat_price']  # €/kWh (th)
+CALORIFIC_VALUE_NGAS = global_config['calorific_value_ngas']  # kWh/m3
 
-# CHP 
-CHP_BONUS_SELF_CONSUMPTION= global_config['chp_bonus_self_consumption']  # €/kWhel
-CHP_BONUS= global_config['chp_bonus']  # €/kWhel
-CHP_INDEX_EEX= global_config['chp_index_eex']  # €/kWhel
-ENERGY_TAX_REFUND_GAS= global_config['energy_tax_refund_gas']  # €/kWhHS
-AVOIDED_GRID_FEES= global_config['avoided_grid_fees']  # €/kWhel
-SHARE_SELF_CONSUMPTION= global_config['share_self_consumption'] # %
-SHARE_FEED_IN= global_config['share_feed_in'] # %
+# CHP
+CHP_BONUS_SELF_CONSUMPTION = global_config['chp_bonus_self_consumption']  # €/kWhel
+CHP_BONUS = global_config['chp_bonus']  # €/kWhel
+CHP_INDEX_EEX = global_config['chp_index_eex']  # €/kWhel
+ENERGY_TAX_REFUND_GAS = global_config['energy_tax_refund_gas']  # €/kWhHS
+AVOIDED_GRID_FEES = global_config['avoided_grid_fees']  # €/kWhel
+SHARE_SELF_CONSUMPTION = global_config['share_self_consumption']  # %
+SHARE_FEED_IN = global_config['share_feed_in']  # %
 
 # Boiler
-POWERCOST_TO_HEAT_SALES_RATIO = global_config['power_cost_to_heat_sales_ratio'] 
+POWERCOST_TO_HEAT_SALES_RATIO = global_config['power_cost_to_heat_sales_ratio']
 
 # Heat Storage
-COST_CHARGE = global_config['cost_charge'] # €/kWh
-COST_DISCHARGE = global_config['cost_discharge'] # €/kWh
+COST_CHARGE = global_config['cost_charge']  # €/kWh
+COST_DISCHARGE = global_config['cost_discharge']  # €/kWh
 
 # Costs
-MAINTENANCE_COSTS = global_config['maintenance_cost'] # €/kWh (HS)
+MAINTENANCE_COSTS = global_config['maintenance_cost']  # €/kWh (HS)
+
 
 class Model:
     """Model class."""
-    
-    def __init__(self):
+
+    def __init__(self, heat_demand_data):
         self.model = AbstractModel()
         self.instance = None
         self.solver = None
         self.timeseries_data = None
         self.results = None
         self.results_data = None
-        self._load_timeseries_data()
+        self._load_timeseries_data(heat_demand_data)
+        self.objective_value = None  # Hinzugefügt: Variable zum Speichern des Zielfunktionswerts
 
     def set_solver(self, solver_name, **kwargs):
         self.solver = SolverFactory(solver_name)
-        
+
         for key in kwargs:
             self.solver.options[key] = kwargs[key]
-    
-    def _load_timeseries_data(self):
-        with open(f'{PATH_IN}demands/{FILE_HEAT_DEMAND}') as f:
-            heat_demand_data = json.load(f)
 
-        t_values = list(map(int, heat_demand_data['heat_demand'].keys()))
-        heat_demand = {int(k): v for k, v in heat_demand_data['heat_demand'].items()}
+    def _load_timeseries_data(self, heat_demand_data):
+        # heat_demand_data: Dictionary mit Heat Demand Daten
+        t_values = list(map(int, heat_demand_data.keys()))
+        heat_demand = {int(k): v for k, v in heat_demand_data.items()}
 
-        self.timeseries_data = { None:{
+        self.timeseries_data = {None: {
             't': {None: t_values},
             'heat_demand': heat_demand
         }}
-
-    # def load_timeseries_data(self):
-    #     """Load timeseries data from file."""
-    #     self.timeseries_data = DataPortal()
-
-    #     self.timeseries_data.load(
-    #         filename=PATH_IN + '/demands/heat_demand_20230401.csv',
-    #         index='t',
-    #         param='heat_demand'
-    #     )
-
-        #_____ In case of power price is not constant _____#
-
-        # self.timeseries_data.load(
-        #     filename=PATH_IN + '/prices/power_price.csv',
-        #     index='t',
-        #     param='power_price'
-        # )
-
-        #_________________________________________________#
-
-        #_____ In case of heat price is not constant _____#
-
-        # self.timeseries_data.load(
-        #     filename=PATH_IN + '/prices/heat_price.csv',
-        #     index='t',
-        #     param='heat_price'
-        # )
-
-        #_________________________________________________#
-
-        #_____ In case of gas price is not constant _____#
-    
-        # self.timeseries_data.load(
-        #     filename=PATH_IN + '/prices/gas_price.csv',
-        #     index='t',
-        #     param='gas_price'
-        # )
-
-        #_________________________________________________#
-
 
     def add_components(self):
         """Add components to the model."""
@@ -146,24 +106,6 @@ class Model:
         self.model.HEAT_PRICE = Param(initialize=HEAT_PRICE)
         self.model.heat_demand = Param(self.model.t)
 
-        #_____ In case of power price is not constant _____#
-        
-        #self.model.power_price = Param(self.model.t)
-
-        #_________________________________________________#
-        
-        #_____ In case of heat price is not constant _____#
-
-        #self.model.heat_price = Param(self.model.t)
-
-        #_________________________________________________#
-
-        #_____ In case of gas price is not constant _____#
-
-        #self.model.gas_price = Param(self.model.t)
-
-        #_________________________________________________#
-
         # Assets
 
         chp1 = chp.Chp(
@@ -174,7 +116,7 @@ class Model:
         )
 
         boiler1 = boiler.Boiler(
-           'boiler1', PATH_IN + '/assets/boiler_operation.csv'
+            'boiler1', PATH_IN + '/assets/boiler_operation.csv'
         )
 
         heat_storage1 = heat_storage.HeatStorage(
@@ -199,34 +141,28 @@ class Model:
         power_grid.add_to_model(self.model)
         heat_grid.add_to_model(self.model)
 
-
     def add_objective(self):
         """Add objective function to model."""
         self.model.objective = Objective(
             rule=self.objective_expr,
             sense=minimize
         )
-    
+
     def instantiate_model(self):
         """Create a concrete instance of the model."""
         self.instance = self.model.create_instance(self.timeseries_data)
-        
-        #Output the model instance to a file
-        with open('output.txt', 'w') as f:
-            self.instance.pprint(ostream=f)
 
-    
     def expand_arcs(self):
         """Expands arcs and generate connection constraints."""
         TransformationFactory('network.expand_arcs').apply_to(self.instance)
-    
+
     def add_instance_components(self, component_name, component):
         """Add components to the instance."""
         self.instance.add_component(component_name, component)
 
     def add_arcs(self):
         """Add arcs to the instance."""
-        
+
         self.instance.arc01 = Arc(
             source=self.instance.chp1.power_out,
             destination=self.instance.power_grid.power_in
@@ -236,7 +172,7 @@ class Model:
             source=self.instance.chp2.power_out,
             destination=self.instance.power_grid.power_in
         )
-        # Updated 
+        # Updated
         self.instance.arc03 = Arc(
             source=self.instance.chp1.heat_out,
             destination=self.instance.heat_grid.heat_in
@@ -271,14 +207,16 @@ class Model:
 
     def solve(self):
         """Solve the model."""
-        self.results =self.solver.solve(
+        self.results = self.solver.solve(
             self.instance,
             symbolic_solver_labels=True,
             tee=True,
             load_solutions=True,
             report_timing=True,
         )
-    
+        # Nach dem Lösen des Modells den Zielfunktionswert speichern
+        self.objective_value = value(self.instance.objective)
+
     def write_results(self):
         """Write results to file."""
         self.results.write()
@@ -291,10 +229,10 @@ class Model:
             name = params.name
             if len(params) == 1:
                 single_value = value(list(params.values())[0])
-                df_params[name]= [single_value for t in self.instance.t]
-            else:                        
+                df_params[name] = [single_value for t in self.instance.t]
+            else:
                 df_params[name] = [value(params[t]) for t in self.instance.t]
-        
+
         for variables in self.instance.component_objects(Var, active=True):
             name = variables.name
             df_variables[name] = [value(variables[t]) for t in self.instance.t]
@@ -304,7 +242,6 @@ class Model:
         df_output.index.name = 't'
 
         self.results_data = df_output
-
 
     def save_results(self, filepath):
         """Save results to object."""
@@ -326,17 +263,17 @@ class Model:
     def _gas_costs(self, model):
         """ Calculate gas costs for CHP and Boiler."""
         gas_costs = (
-        quicksum(model.chp1.gas[t] * model.GAS_PRICE for t in model.t) + 
-        quicksum(model.chp2.gas[t] * model.GAS_PRICE for t in model.t) +
-        quicksum(model.boiler1.gas[t] * model.GAS_PRICE  for t in model.t)
+            quicksum(model.chp1.gas[t] * model.GAS_PRICE for t in model.t) +
+            quicksum(model.chp2.gas[t] * model.GAS_PRICE for t in model.t) +
+            quicksum(model.boiler1.gas[t] * model.GAS_PRICE for t in model.t)
         )
         return gas_costs
-    
+
     def _power_costs(self, model):
-        """Calculate power costs for CHP."""
+        """Calculate power costs for Boiler."""
         power_costs = quicksum(model.boiler1.heat[t] * POWERCOST_TO_HEAT_SALES_RATIO * model.POWER_PRICE for t in model.t)
         return power_costs
-    
+
     # New
     def _storage_costs(self, model):
         """Calculate storage costs for Heat Storage."""
@@ -345,12 +282,12 @@ class Model:
             quicksum(model.heat_storage1.heat_discharge[t] * COST_DISCHARGE for t in model.t)
         )
         return storage_costs
-    
+
     def _maintenance_costs(self, model):
         """Calculate maintenance costs for CHP."""
         maintenance_costs = (
             quicksum(model.chp1.bin[t] * MAINTENANCE_COSTS for t in model.t) +
-            quicksum(model.chp2.bin[t] * MAINTENANCE_COSTS for t in model.t) 
+            quicksum(model.chp2.bin[t] * MAINTENANCE_COSTS for t in model.t)
         )
         return maintenance_costs
 
@@ -361,16 +298,16 @@ class Model:
             quicksum(model.chp2.power[t] * model.POWER_PRICE for t in model.t)
         )
         return power_revenue
-    
+
     def _heat_revenue(self, model):
         """Calculate heat revenue for CHP and Boiler."""
         heat_revenue = (
-        quicksum(model.chp1.heat[t] * model.HEAT_PRICE for t in model.t) +
-        quicksum(model.chp2.heat[t] * model.HEAT_PRICE for t in model.t) +
-        quicksum(model.boiler1.heat[t] * model.HEAT_PRICE for t in model.t)
+            quicksum(model.chp1.heat[t] * model.HEAT_PRICE for t in model.t) +
+            quicksum(model.chp2.heat[t] * model.HEAT_PRICE for t in model.t) +
+            quicksum(model.boiler1.heat[t] * model.HEAT_PRICE for t in model.t)
         )
         return heat_revenue
-    
+
     def _chp_revenue(self, model):
         """Calculate CHP revenue."""
         chp_bonus_for_self_consumption = (
@@ -397,7 +334,7 @@ class Model:
             quicksum(model.chp1.gas[t] * ENERGY_TAX_REFUND_GAS for t in model.t) +
             quicksum(model.chp2.gas[t] * ENERGY_TAX_REFUND_GAS for t in model.t)
         )
-        
+
         chp_revenue = (
             chp_bonus_for_self_consumption +
             chp_bonus_for_feed_in +
@@ -406,13 +343,6 @@ class Model:
             energy_tax_refund
         )
         return chp_revenue
-    
-    def _extract_scenario_date(self, file):
-        """Extract Heat Demand date from the file name."""
-        numbers = re.findall(r'\d+', file)
-        extracted_date = ''.join(numbers)
-        return int(extracted_date)
-
 
 
     def _extract_scenario_info(self, file):
@@ -436,45 +366,121 @@ class Model:
 
 
 if __name__ == "__main__":
-    model = Model()
+    # Flag zum Steuern, ob mehrere Szenarien durchlaufen werden sollen
+    run_multiple_scenarios = False  # Setzen Sie diesen Wert auf False, um nur ein Szenario zu laufen
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    log_filename = f"{PATH_OUT_LOGS}logile_{timestamp}.log"
+    if run_multiple_scenarios:
+        # Laden der Heat-Demand-Szenarien
+        with open(f'{PATH_IN}demands/{FILE_HEAT_DEMAND_SCENARIOS}') as f:
+            heat_demand_scenarios = json.load(f)
 
-    print('Setting solver...')
-    model.set_solver(
-        solver_name= 'gurobi',
-        MIPGap=0.015,
-        TimeLimit=20,
-        LogFile= log_filename
+        # Entfernen der "Probability" Einträge aus den Szenarien
+        for scenario in heat_demand_scenarios:
+            if 'Probability' in heat_demand_scenarios[scenario]:
+                del heat_demand_scenarios[scenario]['Probability']
+
+        # Liste zur Speicherung der Zielfunktionswerte
+        objective_values = []
+    
+        # Iteration über alle Szenarien
+        for scenario_name, heat_demand_data in heat_demand_scenarios.items():
+            print(f'\n### Running scenario: {scenario_name} ###\n')
+
+            model = Model(heat_demand_data)
+
+            start_date, end_date, period = model._extract_scenario_info(FILE_HEAT_DEMAND)
+
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            log_filename = f"{PATH_OUT_LOGS}logfile_{scenario_name}_{timestamp}_{start_date}_{period}.log"
+
+            print('Setting solver...')
+            model.set_solver(
+                solver_name='gurobi',
+                MIPGap=0.015,
+                TimeLimit=20,
+                LogFile=log_filename
+            )
+
+            print('Adding components...')
+            model.add_components()
+
+            print('Adding objective...')
+            model.add_objective()
+
+            print('Instantiating model...')
+            model.instantiate_model()
+
+            print('Declaring arcs...')
+            model.add_arcs()
+            model.expand_arcs()
+
+            print('Solving model...')
+            model.solve()
+
+            # Zielfunktionswert speichern
+            objective_value = model.objective_value
+            objective_values.append({'Scenario': scenario_name, 'ObjectiveValue': objective_value})
+
+            print('Writing results...')
+            model.write_results()
+
+            # Speichern der Ergebnisse mit Szenarioname im Dateinamen
+            output_file = f'd_{scenario_name}_{start_date}_to_{end_date}_{period}_ts.csv'
+            model.save_results(PATH_OUT_TIMESERIES + output_file)
+
+        # Speichern der Zielfunktionswerte in einer separaten CSV-Datei
+        df_objectives = pd.DataFrame(objective_values)
+        objectives_file = f'{PATH_OUT_OBJECTIVES}d_scenarios_{start_date}_to_{end_date}_{period}_obj.csv'
+        df_objectives.to_csv(objectives_file, index=False)
+
+        print(f'\n### All scenarios have been processed. Objective values saved to {objectives_file} ###')
+
+    else:
+        # Ein einzelnes Szenario ausführen
+        # Laden des Heat Demands aus der ursprünglichen Datei
+        with open(f'{PATH_IN}demands/{FILE_HEAT_DEMAND}') as f:
+            heat_demand_data = json.load(f)
+
+        # Falls die Daten in einem speziellen Format vorliegen (z.B. unter 'heat_demand'), anpassen
+        if 'heat_demand' in heat_demand_data:
+            heat_demand_data = heat_demand_data['heat_demand']
+
+        model = Model(heat_demand_data)
+
+        start_date, end_date, period = model._extract_scenario_info(FILE_HEAT_DEMAND)
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        log_filename = f"{PATH_OUT_LOGS}logfile_{timestamp}.log"
+
+        print('Setting solver...')
+        model.set_solver(
+            solver_name='gurobi',
+            MIPGap=0.015,
+            TimeLimit=20,
+            LogFile=log_filename
         )
 
-    # print('Loading timeseries data...')
-    # model.load_timeseries_data()
+        print('Adding components...')
+        model.add_components()
 
-    print('Adding components...')
-    model.add_components()
+        print('Adding objective...')
+        model.add_objective()
 
-    print('Adding objective...')
-    model.add_objective()
+        print('Instantiating model...')
+        model.instantiate_model()
 
-    print('Instantiating model...')
-    model.instantiate_model()
+        print('Declaring arcs...')
+        model.add_arcs()
+        model.expand_arcs()
 
-    print('Declairing arcs...')
-    model.add_arcs()
-    model.expand_arcs()
+        print('Solving model...')
+        model.solve()
 
-    
-    print('Solving model...')
-    model.solve()
+        print('Writing results...')
+        model.write_results()
 
-        
-    
-    print('Writing results...')
-    model.write_results()
-    start_date, end_date, period = model._extract_scenario_info(FILE_HEAT_DEMAND)
-    output_file = f'd_{start_date}_to_{end_date}_{period}_ts.csv'
-    model.save_results(PATH_OUT_TIMESERIES +  output_file)
-    
-    
+        # Speichern der Ergebnisse
+        output_file = f'd_{start_date}_to_{end_date}_{period}_ts.csv'
+        model.save_results(PATH_OUT_TIMESERIES + output_file)
+
+        print('\n### Single scenario has been processed. ###')
